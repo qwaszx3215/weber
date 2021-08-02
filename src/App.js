107a9cbe711238c8e570d921b47c6ee3bd3582bd
qwaszx3215 <fredrick3smith33@gmail.com>
2021-08-02 06:59:53 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ContactUs from './components/contactForm';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <ContactUs/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
